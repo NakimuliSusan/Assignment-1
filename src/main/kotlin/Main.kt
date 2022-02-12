479acbbd4a639e0aa42267d17ae2640282f752f5
mitchelle-hope <mitchellehopewaithera@gmail.com>
2022-02-12 12:53:57 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+fun main() {
+    var name = "Nakimuli Mary Susan"
+    println(name)
+    var age = "22"
+    println(age)
+    var  weight =(55)
+    println(weight)
+    var phonenumber = "0706112100"
+    println(phonenumber)
+    var citizenShip = "Ugandan"
+    println(citizenShip)
+    val isUgandan: Boolean = true
+    println(isUgandan) // output true
+    val isKenyan: Boolean = false
+    println(isKenyan) // output false
+}
